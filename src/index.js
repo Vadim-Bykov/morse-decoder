439c584bc16934e06d27b1175f1bfb853416ba63
Vadim Bykov <bvntaev@gmail.com>
2020-10-07 11:06:22 +0300

    feat: solve morse code decoder

--- src/index.js
+++ src/index.js
@@ -1,44 +1,62 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+    expr = expr.split('')
+    let arr = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        const arrDown = expr.slice(i, i + 10);
+        // console.log(arrDown)
+        let newArrDown = [];
+        for (let j = 0; j < arrDown.length; j += 2) {
+            if (arrDown[j].includes('*')) continue
+            const doubleArr = arrDown.slice(j, j + 2).join('')
+            // console.log(doubleArr)
+            newArrDown.push(doubleArr)
+        }
+        newArrDown = newArrDown.filter(item => item === '10' || item === '11').map(item => item === '10' ? item = '.' : item = '-').join('')
+        arr.push(newArrDown)
+    }
+    // console.log(arr)
+    const resStr = arr.map(item => item === '' ? item = ' ' : item = MORSE_TABLE[item]).join('')
+    // console.log(resStr)
+    return resStr
 }
 
 module.exports = {
